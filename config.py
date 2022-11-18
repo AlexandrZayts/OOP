15942f100da9c2c0923b13de84539cb15917e21d
AlexandrZayts <sanya9-1998@inbox.ru>
2022-11-18 15:55:55 +0300

    1

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = "5665739826:AAEhRH8dJFhnNa3QI9SqfeMe07kItxSrogI"
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
---
+++ utils.py
@@ -0,0 +1,34 @@
+import requests
+import json
+from config import keys
+
+
+class ConvertionException(Exception):
+    pass
+
+
+class ValuesConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise ConvertionException(f'Невозможно перевести одинаковые валюты {base}.')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {quote}')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise ConvertionException(f'Не удалось обработать валюту {base}')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise ConvertionException(f'Не удалось обработать количество {amount}')
+
+        r = requests.get(f'https://api.tinkoff.ru/v1/currency_rates?from={quote_ticker}&to={base_ticker}')
+        total_base = json.loads(r.content)[keys[base]]
+
+        return total_base
